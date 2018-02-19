1ff742e3a96f803f789f33d56c5a02e8faf3fac1
J. Tran <jyntran@gmail.com>
2018-02-18 23:22:47 -0500

    Basic Poogie and digital time

--- src/c/poogie.c
+++ src/c/poogie.c
@@ -1,43 +1,50 @@
 #include <pebble.h>
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static TextLayer *s_time_layer;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_bitmap_poogie;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(s_time_layer, s_buffer);
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  s_background_layer = bitmap_layer_create(GRect(0, 25, bounds.size.w, 75));
+  bitmap_layer_set_compositing_mode(s_background_layer, GCompOpSet);
+  s_bitmap_poogie = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_POOGIE);
+  bitmap_layer_set_bitmap(s_background_layer, s_bitmap_poogie);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  s_time_layer = text_layer_create(GRect(0, bounds.size.h-75, bounds.size.w, 75));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_36_BOLD_NUMBERS));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+  gbitmap_destroy(s_bitmap_poogie);
+  text_layer_destroy(s_time_layer);
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void prv_init(void) {
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
     .unload = prv_window_unload,
